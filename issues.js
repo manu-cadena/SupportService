1164f975f025deb64337baecf5a0b86d02bb0573
Manuel <69732401+manu-cadena@users.noreply.github.com>
2025-02-10 15:03:46 +0100

    Updated user interface for issue list

--- issues.js
+++ issues.js
@@ -1,13 +1,30 @@
 import { HttpClient } from './helpers/httpClient.js';
+
+const issueList = document.querySelector('#issueList');
 const initApp = () => {
   listAllIssues();
 };
 
 const listAllIssues = async () => {
   const httpClient = new HttpClient();
-
   const result = await httpClient.get('issues');
+  displayIssues(result);
   console.log(result);
 };
 
+const displayIssues = (issues) => {
+  for (let issue of issues) {
+    const div = document.createElement('div');
+    const caption = document.createElement('span');
+    const issueDate = document.createElement('span');
+    const issuer = document.createElement('span');
+    caption.textContent = issue.caption;
+    issueDate.textContent = issue.issueDate;
+    issuer.textContent = issue.email;
+    div.appendChild(issueDate);
+    div.appendChild(caption);
+    div.appendChild(issuer);
+    issueList.appendChild(div);
+  }
+};
 document.addEventListener('DOMContentLoaded', initApp);
